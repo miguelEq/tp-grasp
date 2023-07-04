1e2ff6458a2716edc0877b93d1690393a6c96ac8
miguelEq <miguelequilea18@gmail.com>
2023-07-03 22:40:19 -0300

    agrego greedy randomized

--- index.js
+++ index.js
@@ -34,8 +34,9 @@ aristas =
 
 const getAristas = (matriz, verticeInicio) => {
     let aristas = []
-    matriz[verticeInicio].forEach(v => {
-        if (verticeDestino != vertice) {
+    console.log(matriz, verticeInicio)
+    matriz[verticeInicio].forEach((v,verticeDestino) => {
+        if (verticeDestino != verticeInicio) {
             aristas.push({verticeOrigen:verticeInicio, verticeDestino: verticeDestino, peso:matriz[verticeInicio][verticeDestino]})
         }
     });
@@ -43,7 +44,7 @@ const getAristas = (matriz, verticeInicio) => {
 }
 
 function greedyRandomized(matriz, vertices) {
-    let actual = vertices[Math.floor(Math.random() * (vertices.length))]  
+    let actual = Math.floor(Math.random() * (vertices.length))  
     const visitados = new Array(vertices.length).fill(false); 
     visitados[actual] = true;
     let totalVisitados = 1;
@@ -54,13 +55,21 @@ function greedyRandomized(matriz, vertices) {
             .sort((a,b) => {
                 return a.peso - b.peso
             })
-        const siguiente = obtenerRandom(aristas)
+        console.log("aristas -----")
+        console.log(aristas)
+        const siguiente = aristas[obtenerRandom(aristas)]
+        console.log("arista random -----")
+        console.log(siguiente)
         res.push(siguiente)
         actual = siguiente.verticeDestino
         visitados[actual] = true
         totalVisitados++
     }
-    //completo el circuito agregando la arista que regresa al inicio
+    //agrego la arista que regresa al vertice inicial para completar el circuito
+    const aristaInicio = res[0].verticeOrigen 
+    const aristaFin = res[res.length-1].verticeDestino
+    res.push({verticeOrigen:aristaFin, verticeDestino: aristaInicio, peso:matriz[aristaFin][aristaInicio]}) 
+    console.log(res)
 } 
 
 function obtenerRandom(adyacentes){
@@ -69,4 +78,14 @@ function obtenerRandom(adyacentes){
 	return randomAdyacente
 }
 
-
+function grasp(){
+    const ejemplo = [
+        [0,3,1,5],
+        [3,0,1,4],
+        [1,1,0,1],
+        [5,4,1,0]
+    ]
+    const vertices = [4,3,5,8]
+    greedyRandomized(ejemplo,vertices)
+}
+grasp()
